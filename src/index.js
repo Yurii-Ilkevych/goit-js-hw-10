599213f307082e0bd2dfe68ee5026a32fc54af9b
Yurii-Ilkevych <rufiktcl@gmail.com>
2023-04-15 19:27:57 +0300

    add common fails

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './css/styles.css';
+
+const DEBOUNCE_DELAY = 300;
+const refs = {
+    searchBox: document.querySelector("#search-box"),
+    countryList: document.querySelector(".country-list"),
+    countryInfo: document.querySelector(".country-info"),
+}
+
+console.log(refs)
